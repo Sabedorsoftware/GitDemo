d98c9ab5985641e07331fa4a66e83661793f9d8e
Aniruddha <aniruddha@gmail.com>
2025-07-17 09:19:55 +0530

    second commit by Jeff

--- src/Test.java
+++ src/Test.java
@@ -7,6 +7,10 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Hi");
+		System.out.println("Helloo");
+		
 		System.out.println("Hello From Selenium");
 		
 		//WebDriver driver=new FirefoxDriver();
